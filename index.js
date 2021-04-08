919f2564d259df9dcabf82e8f95bef4d54403c7b
rohittz <r_o_h_i_t_roy@yahoo.com>
2021-04-08 16:31:34 +0600

    not working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
-const app = express()
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
