6565bd8f9cb4b0a73a3bda390bcce40a438e434d
rohittz <r_o_h_i_t_roy@yahoo.com>
2021-04-08 16:47:12 +0600

    done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-const port = process.env.PORT || 3005;
+const port = 3005;
 //mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dipq6.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
